030e7ca5edf5efb0518a8b555519ad55fff66442
hikarumurayama <murayama.hikaru@t-clue.com>
2021-01-31 14:56:37 +0900

    props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
 
 function App() {
-  return <div><h1>Hello,world!</h1></div>;
+  const profiles = [
+    {name:"Taro",age:10},
+    {name:"Hanako",age:12},
+    {name:"Takeshi"},
+  ]
+  return (
+    <div>
+      {
+        profiles.map((profile,index)=>{
+          return <User name={"profile.name"} age={profile.age} key={index}/>
+        })
+      }
+    </div>
+  )
+}
+
+const User = (props) =>{
+  return <div>Hi! I am {props.name},and {props.age} years old!</div>
+}
+
+User.defaultProps ={
+  age:1
 }
 
 export default App;
