7722012c34b85dbe006437482e63dcab9e6ba0c6
hikarumurayama <usagi.to.akari@gmail.com>
2021-02-25 09:30:40 +0900

    fix state

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const App = ()=> (<Counter></Counter>)
 class Counter extends Component{
   
   //初期化処理
-  constructor(props){
+  constructor(props){　
     super(props)
     console.log(this.state)
     this.state= {count:0}
@@ -13,8 +13,10 @@ class Counter extends Component{
 
   // プラスボタン
   handlePlusButton = () =>{
-    this.state = { count: this.state.count + 1}
-    //this.setState({ count: this.state.count + 1})
+    // 以下は絶対にやってはいけない
+    //this.state = { count: this.state.count + 1} 
+
+    this.setState({ count: this.state.count + 1})
   }
   
   // マイナスボタン
