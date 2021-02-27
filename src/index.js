4755f4cf9956356abaed78a9d469a4e1713ce6e0
hikarumurayama <usagi.to.akari@gmail.com>
2021-02-27 10:30:26 +0900

    implement store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import './index.css';
-import App from './App';
+import reducer from './reducers'
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+
+const store = creaeStore(reducer)
 
 ReactDOM.render(
   <React.StrictMode>
