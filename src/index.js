a8479530cce408cb366d5574aff276bcb68e6d12
hikarumurayama <usagi.to.akari@gmail.com>
2021-03-04 07:43:48 +0900

    implement event new component

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import './index.css';
 import reducer from './reducers'
 import EventsIndex from './components/events_index';
+import EventsNew from './components/events_new';
 import reportWebVitals from './reportWebVitals';
 
 // thunkを利用するために、apllyMiddlewareに引数として渡し、その返り値をcreateStoreに渡す
@@ -13,7 +15,12 @@ const store = createStore(reducer,applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
-    <EventsIndex />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/events/new" component={EventsNew} />
+        <Route exact path="/" component={EventsIndex} />
+      </Switch>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
